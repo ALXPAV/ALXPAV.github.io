e8acaf765471dfa3fdd641c698dc29759e3edd80
ALXPAV <125505598+ALXPAV@users.noreply.github.com>
2024-06-22 11:42:51 +0300

    Update run.php

--- run.php
+++ run.php
@@ -1,4 +1,12 @@
 <?php
-$output = shell_exec('./OKRun.exe');
-echo "Результат выполнения: " . $output;
+$os = PHP_OS_FAMILY;
+
+if ($os === 'Windows') {
+    $output = shell_exec('.\\OKRun.exe');
+} else {
+    $output = 'Этот сервер не работает под управлением Windows.';
+}
+
+echo "Операционная система: $os\nРезультат выполнения: $output";
 ?>
+
